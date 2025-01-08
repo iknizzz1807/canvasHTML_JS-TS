8d3a4135b0279696a79a196aed0f318de8ffd6cf
Nguyen My Thong <90298623+iknizzz1807@users.noreply.github.com>
2025-01-09 02:48:32 +0700

    add eraser

--- blackBoard/script.js
+++ blackBoard/script.js
@@ -18,11 +18,17 @@ class LineTool extends DrawingTool {
   }
 
   onMouseMove(x, y) {
-    this.ctx.strokeStyle = "white";
-    this.ctx.lineTo(x, y);
-    this.ctx.stroke();
-    this.ctx.beginPath();
-    this.ctx.moveTo(x, y);
+    if (this.isMousePressed) {
+      this.ctx.strokeStyle = "white";
+      this.ctx.lineTo(x, y);
+      this.ctx.stroke();
+      this.ctx.beginPath();
+      this.ctx.moveTo(x, y);
+    }
+  }
+
+  onMouseUp() {
+    this.ctx.closePath();
   }
 }
 
@@ -44,14 +50,16 @@ class SquareTool extends DrawingTool {
   }
 
   onMouseMove(x, y) {
-    this.ctx.putImageData(this.canvasState, 0, 0);
-    this.ctx.beginPath();
-    this.ctx.strokeRect(
-      this.startPos.x,
-      this.startPos.y,
-      x - this.startPos.x,
-      y - this.startPos.y
-    );
+    if (this.isMousePressed) {
+      this.ctx.putImageData(this.canvasState, 0, 0);
+      this.ctx.beginPath();
+      this.ctx.strokeRect(
+        this.startPos.x,
+        this.startPos.y,
+        x - this.startPos.x,
+        y - this.startPos.y
+      );
+    }
   }
 
   onMouseUp() {
@@ -64,26 +72,48 @@ class SquareTool extends DrawingTool {
   }
 }
 
+class EraserTool extends DrawingTool {
+  onMouseDown(x, y) {
+    this.erase(x, y);
+  }
+
+  onMouseMove(x, y) {
+    if (this.isMousePressed) {
+      this.erase(x, y);
+    }
+  }
+
+  onMouseUp() {}
+
+  erase(x, y) {
+    this.ctx.clearRect(x - 10, y - 10, 20, 20); // Erase a 20x20 square
+  }
+}
+
 class DrawingApp {
   constructor() {
     this.canvas = document.getElementById("canvas");
     this.ctx = this.canvas.getContext("2d");
+    this.oldWidth = 0;
+    this.oldHeight = 0;
+    this.setupEventListeners();
     this.setupCanvas();
     this.setupTools();
-    this.setupEventListeners();
   }
 
   setupCanvas() {
     this.ctx.lineWidth = 2;
     this.ctx.lineCap = "round";
     this.ctx.strokeStyle = "white";
-    this.resizeCanvas();
+    this.canvas.height = window.innerHeight - 8;
+    this.canvas.width = window.innerWidth - 100;
   }
 
   setupTools() {
     this.tools = {
       line: new LineTool(this.canvas, this.ctx),
       square: new SquareTool(this.canvas, this.ctx),
+      eraser: new EraserTool(this.canvas, this.ctx),
     };
     this.currentTool = this.tools.line;
   }
@@ -97,6 +127,10 @@ class DrawingApp {
 
     document.getElementById("square-btn").addEventListener("click", () => {
       this.currentTool = this.tools.square;
+    });
+
+    document.getElementById("eraser-btn").addEventListener("click", () => {
+      this.currentTool = this.tools.eraser;
     });
 
     document.getElementById("clear-btn").addEventListener("click", () => {
@@ -137,8 +171,20 @@ class DrawingApp {
   }
 
   resizeCanvas() {
+    // Save the current canvas content
+    const imageData = this.ctx.getImageData(
+      0,
+      0,
+      this.canvas.width,
+      this.canvas.height
+    );
+
+    // Resize the canvas
     this.canvas.height = window.innerHeight - 8;
     this.canvas.width = window.innerWidth - 100;
+
+    // Restore the canvas content
+    this.ctx.putImageData(imageData, 0, 0);
   }
 
   clearCanvas() {
