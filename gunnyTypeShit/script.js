2c70c1efd909f9c2eee54fbd47b5224e74815569
Nguyen My Thong <90298623+iknizzz1807@users.noreply.github.com>
2025-01-15 02:37:38 +0700

    Finish gunny

--- gunnyTypeShit/script.js
+++ gunnyTypeShit/script.js
@@ -6,30 +6,121 @@ var Vector2D = /** @class */ (function () {
     return Vector2D;
 }());
 var Bullet = /** @class */ (function () {
-    function Bullet() {
+    function Bullet(startPos, angle, power, wind) {
+        this.gravity = 0.5;
+        this.isActive = false;
+        this.position = new Vector2D(startPos.x, startPos.y);
+        var radian = (angle * Math.PI) / 180;
+        this.velocity = new Vector2D((Math.cos(radian) * power) / 10, (-Math.sin(radian) * power) / 10);
+        this.wind = wind;
     }
+    Bullet.prototype.update = function () {
+        if (!this.isActive)
+            return;
+        // Apply wind force
+        this.velocity.x += this.wind * 0.01;
+        // Apply gravity
+        this.velocity.y += this.gravity;
+        // Update position
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
+    };
+    Bullet.prototype.draw = function (ctx) {
+        if (!this.isActive)
+            return;
+        ctx.fillStyle = "red";
+        ctx.beginPath();
+        ctx.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2);
+        ctx.fill();
+    };
+    Bullet.prototype.shoot = function () {
+        this.isActive = true;
+    };
+    Bullet.prototype.isOutOfBounds = function (width, height) {
+        return (this.position.x < 0 ||
+            this.position.x > width ||
+            this.position.y < 0 ||
+            this.position.y > height);
+    };
+    Bullet.prototype.getPosition = function () {
+        return this.position;
+    };
+    Bullet.prototype.isShot = function () {
+        return this.isActive;
+    };
     return Bullet;
+}());
+var Enemy = /** @class */ (function () {
+    function Enemy(pos) {
+        this.width = 32;
+        this.height = 32;
+        this.health = 100;
+        this.position = pos;
+    }
+    Enemy.prototype.draw = function (ctx) {
+        // Draw enemy
+        ctx.fillStyle = "red";
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        // Draw health bar
+        ctx.fillStyle = "black";
+        ctx.fillRect(this.position.x, this.position.y - 20, this.width, 10);
+        ctx.fillStyle = "green";
+        ctx.fillRect(this.position.x, this.position.y - 20, (this.width * this.health) / 100, 10);
+    };
+    Enemy.prototype.takeDamage = function (damage) {
+        this.health = Math.max(0, this.health - damage);
+    };
+    Enemy.prototype.checkCollision = function (bulletPos) {
+        return (bulletPos.x > this.position.x &&
+            bulletPos.x < this.position.x + this.width &&
+            bulletPos.y > this.position.y &&
+            bulletPos.y < this.position.y + this.height);
+    };
+    Enemy.prototype.isDead = function () {
+        return this.health <= 0;
+    };
+    return Enemy;
 }());
 var Player = /** @class */ (function () {
     function Player(spawnPos) {
+        this.bullet = null;
         this.position = spawnPos;
         this.width = 32;
         this.height = 32;
-        this.canShoot = true;
     }
-    Player.prototype.shoot = function () {
-        if (this.canShoot) {
-            this.canShoot = false;
-            // shoot
-        }
+    Player.prototype.shoot = function (angle, power, wind) {
+        if (this.bullet && this.bullet.isShot())
+            return false;
+        this.bullet = new Bullet(new Vector2D(this.position.x + this.width / 2, this.position.y), angle, power, wind);
+        this.bullet.shoot();
+        return true;
     };
-    Player.prototype.move = function (direction) {
-        if (direction === "left") {
-            this.position.x -= 2;
-        }
-        else if (direction === "right") {
-            this.position.x += 2;
-        }
+    Player.prototype.update = function () {
+        var _a;
+        (_a = this.bullet) === null || _a === void 0 ? void 0 : _a.update();
+    };
+    Player.prototype.draw = function (ctx) {
+        var _a;
+        // Draw player
+        ctx.fillStyle = "blue";
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        // Draw bullet
+        (_a = this.bullet) === null || _a === void 0 ? void 0 : _a.draw(ctx);
+    };
+    Player.prototype.getBulletPosition = function () {
+        var _a;
+        return ((_a = this.bullet) === null || _a === void 0 ? void 0 : _a.getPosition()) || null;
+    };
+    Player.prototype.isBulletActive = function () {
+        var _a;
+        return ((_a = this.bullet) === null || _a === void 0 ? void 0 : _a.isShot()) || false;
+    };
+    Player.prototype.checkBulletOutOfBounds = function (width, height) {
+        var _a;
+        return ((_a = this.bullet) === null || _a === void 0 ? void 0 : _a.isOutOfBounds(width, height)) || false;
+    };
+    Player.prototype.resetBullet = function () {
+        this.bullet = null;
     };
     Player.prototype.getPosition = function () {
         return this.position;
@@ -39,6 +130,18 @@ var Player = /** @class */ (function () {
     };
     Player.prototype.getHeight = function () {
         return this.height;
+    };
+    Player.prototype.moveLeft = function () {
+        this.position.x -= 10;
+    };
+    Player.prototype.moveRight = function () {
+        this.position.x += 10;
+    };
+    Player.prototype.moveUp = function () {
+        this.position.y -= 10;
+    };
+    Player.prototype.moveDown = function () {
+        this.position.y += 10;
     };
     return Player;
 }());
@@ -54,15 +157,55 @@ var Game = /** @class */ (function () {
         this.degree = 0;
         this.power = 0;
         this.maxDegree = 180;
-        this.maxPower = 100;
+        this.maxPower = 500;
+        this.powerDirection = 1;
+        this.wind = 0;
         window.addEventListener("keydown", function (e) {
-            if (e.key === " ") {
-                if (_this.power < _this.maxPower)
-                    _this.power += 2;
+            if (e.key === "ArrowUp") {
+                if (_this.degree < _this.maxDegree) {
+                    _this.degree += 5;
+                }
+            }
+            else if (e.key === "ArrowDown") {
+                if (_this.degree > 0) {
+                    _this.degree -= 5;
+                }
+            }
+            else if (e.key === " ") {
+                e.preventDefault();
+                if (_this.power >= _this.maxPower) {
+                    _this.powerDirection = -1;
+                }
+                else if (_this.power <= 0) {
+                    _this.powerDirection = 1;
+                }
+                if (_this.powerDirection === 1)
+                    _this.power += 10;
+                else if (_this.powerDirection === -1)
+                    _this.power -= 10;
+            }
+            else if (e.key === "w") {
+                _this.player.moveUp();
+            }
+            else if (e.key === "a") {
+                _this.player.moveLeft();
+            }
+            else if (e.key === "s") {
+                _this.player.moveDown();
+            }
+            else if (e.key === "d") {
+                _this.player.moveRight();
             }
         });
-        this.drawBackGround();
+        var shootButton = document.getElementById("shootButton");
+        shootButton === null || shootButton === void 0 ? void 0 : shootButton.addEventListener("click", function () {
+            if (_this.player.shoot(_this.degree, _this.power, _this.wind)) {
+                // Generate new random wind after successful shot
+                _this.wind = (Math.random() - 0.5) * 2; // Random value between -1 and 1
+            }
+        });
         this.player = new Player(new Vector2D(100, this.canvas_height - 200));
+        this.enemy = new Enemy(new Vector2D(this.canvas_width - 200, this.canvas_height - 200));
         this.gameLoop = this.gameLoop.bind(this);
         requestAnimationFrame(this.gameLoop);
     }
@@ -72,31 +215,74 @@ var Game = /** @class */ (function () {
             this.ctx.fillRect(0, 0, this.canvas_width, this.canvas_height);
         }
     };
-    Game.prototype.drawPlayer = function () {
+    Game.prototype.drawWind = function () {
         if (this.ctx) {
-            var pos = this.player.getPosition();
-            var width = this.player.getWidth();
-            var height = this.player.getHeight();
-            this.ctx.fillStyle = "blue";
-            this.ctx.fillRect(pos.x, pos.y, width, height);
+            this.ctx.fillStyle = "white";
+            this.ctx.font = "20px Arial";
+            this.ctx.fillText("".concat(this.wind.toFixed(2)), 20, 30);
+            // Draw wind direction arrow
+            var arrowLength = Math.abs(this.wind) * 50;
+            var arrowX = 100;
+            var arrowY = 25;
+            this.ctx.beginPath();
+            this.ctx.strokeStyle = "white";
+            if (this.wind > 0) {
+                this.ctx.fillStyle = "green";
+                this.ctx.fillText(">>>>>>>>>>>", arrowX, arrowY + 5);
+            }
+            else {
+                this.ctx.fillStyle = "green";
+                this.ctx.fillText("<<<<<<<<<<<", arrowX, arrowY + 5);
+            }
+            this.ctx.stroke();
         }
     };
     Game.prototype.drawPower = function () {
         if (this.ctx) {
-            this.ctx.fillStyle = "green";
-            this.ctx.fillRect(240, this.canvas_height - 40, this.power * 10, 20);
+            this.powerDirection === 1
+                ? (this.ctx.fillStyle = "green")
+                : (this.ctx.fillStyle = "orange");
+            this.ctx.fillRect(240, this.canvas_height - 40, this.power * 2, 20);
             this.ctx.strokeStyle = "white";
-            this.ctx.strokeRect(240, this.canvas_height - 40, this.maxPower * 10, 20);
+            this.ctx.strokeRect(240, this.canvas_height - 40, this.maxPower * 2, 20);
         }
     };
     Game.prototype.drawDegree = function () {
         if (this.ctx) {
+            var radius = 80;
             this.ctx.beginPath();
-            this.ctx.arc(120, this.canvas_height - 20, 80, Math.PI, 2 * Math.PI);
-            this.ctx.fillStyle = "transparent";
-            this.ctx.fill();
+            this.ctx.strokeStyle = "white";
+            this.ctx.arc(120, this.canvas_height - 20, radius, Math.PI, 2 * Math.PI);
             this.ctx.lineWidth = 5;
             this.ctx.stroke();
+            this.ctx.fillStyle = "black";
+            this.ctx.font = "20px arial";
+            this.ctx.fillText("90", 110, this.canvas_height - 80);
+            this.ctx.fillText("180", 44, this.canvas_height - 24);
+            this.ctx.fillText("0", 184, this.canvas_height - 24);
+            this.ctx.beginPath();
+            this.ctx.strokeStyle = "red";
+            var radian = (this.degree * Math.PI) / 180;
+            var centerX = 120;
+            var centerY = this.canvas_height - 22;
+            var x = centerX + radius * Math.cos(radian);
+            var y = centerY - radius * Math.sin(radian);
+            this.ctx.moveTo(centerX, centerY);
+            this.ctx.lineTo(x, y);
+            this.ctx.stroke();
+        }
+    };
+    Game.prototype.checkCollisions = function () {
+        if (!this.ctx)
+            return;
+        var bulletPos = this.player.getBulletPosition();
+        if (bulletPos && this.enemy.checkCollision(bulletPos)) {
+            this.enemy.takeDamage(20);
+            this.player.resetBullet();
+            if (this.enemy.isDead()) {
+                alert("You won!");
+                // Reset game or handle win condition
+            }
         }
     };
     Game.prototype.clearCanvas = function () {
@@ -104,7 +290,16 @@ var Game = /** @class */ (function () {
     };
     Game.prototype.gameLoop = function () {
         this.clearCanvas();
-        this.drawPlayer();
+        // Update
+        this.player.update();
+        if (this.player.checkBulletOutOfBounds(this.canvas_width, this.canvas_height)) {
+            this.player.resetBullet();
+        }
+        this.checkCollisions();
+        // Draw
+        this.drawWind();
+        this.player.draw(this.ctx);
+        this.enemy.draw(this.ctx);
         this.drawPower();
         this.drawDegree();
         requestAnimationFrame(this.gameLoop);
--- gunnyTypeShit/script.ts
+++ gunnyTypeShit/script.ts
@@ -10,34 +10,163 @@ class Vector2D {
 
 class Bullet {
   private position: Vector2D;
+  private velocity: Vector2D;
+  private gravity: number = 0.5;
+  private wind: number;
+  private isActive: boolean = false;
+
+  constructor(startPos: Vector2D, angle: number, power: number, wind: number) {
+    this.position = new Vector2D(startPos.x, startPos.y);
+    const radian = (angle * Math.PI) / 180;
+    this.velocity = new Vector2D(
+      (Math.cos(radian) * power) / 10,
+      (-Math.sin(radian) * power) / 10
+    );
+    this.wind = wind;
+  }
+
+  update() {
+    if (!this.isActive) return;
+
+    // Apply wind force
+    this.velocity.x += this.wind * 0.01;
+
+    // Apply gravity
+    this.velocity.y += this.gravity;
+
+    // Update position
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    if (!this.isActive) return;
+
+    ctx.fillStyle = "red";
+    ctx.beginPath();
+    ctx.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2);
+    ctx.fill();
+  }
+
+  shoot() {
+    this.isActive = true;
+  }
+
+  isOutOfBounds(width: number, height: number): boolean {
+    return (
+      this.position.x < 0 ||
+      this.position.x > width ||
+      this.position.y < 0 ||
+      this.position.y > height
+    );
+  }
+
+  getPosition() {
+    return this.position;
+  }
+
+  isShot() {
+    return this.isActive;
+  }
+}
+
+class Enemy {
+  private position: Vector2D;
+  private width: number = 32;
+  private height: number = 32;
+  private health: number = 100;
+
+  constructor(pos: Vector2D) {
+    this.position = pos;
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    // Draw enemy
+    ctx.fillStyle = "red";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    // Draw health bar
+    ctx.fillStyle = "black";
+    ctx.fillRect(this.position.x, this.position.y - 20, this.width, 10);
+    ctx.fillStyle = "green";
+    ctx.fillRect(
+      this.position.x,
+      this.position.y - 20,
+      (this.width * this.health) / 100,
+      10
+    );
+  }
+
+  takeDamage(damage: number) {
+    this.health = Math.max(0, this.health - damage);
+  }
+
+  checkCollision(bulletPos: Vector2D): boolean {
+    return (
+      bulletPos.x > this.position.x &&
+      bulletPos.x < this.position.x + this.width &&
+      bulletPos.y > this.position.y &&
+      bulletPos.y < this.position.y + this.height
+    );
+  }
+
+  isDead(): boolean {
+    return this.health <= 0;
+  }
 }
 
 class Player {
   private position: Vector2D;
-  private canShoot: boolean;
   private width: number;
   private height: number;
+  private bullet: Bullet | null = null;
 
   constructor(spawnPos: Vector2D) {
     this.position = spawnPos;
     this.width = 32;
     this.height = 32;
-    this.canShoot = true;
   }
 
-  shoot() {
-    if (this.canShoot) {
-      this.canShoot = false;
-      // shoot
-    }
+  shoot(angle: number, power: number, wind: number) {
+    if (this.bullet && this.bullet.isShot()) return false;
+
+    this.bullet = new Bullet(
+      new Vector2D(this.position.x + this.width / 2, this.position.y),
+      angle,
+      power,
+      wind
+    );
+    this.bullet.shoot();
+    return true;
   }
 
-  move(direction: "left" | "right") {
-    if (direction === "left") {
-      this.position.x -= 2;
-    } else if (direction === "right") {
-      this.position.x += 2;
-    }
+  update() {
+    this.bullet?.update();
+  }
+
+  draw(ctx: CanvasRenderingContext2D) {
+    // Draw player
+    ctx.fillStyle = "blue";
+    ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    // Draw bullet
+    this.bullet?.draw(ctx);
+  }
+
+  getBulletPosition(): Vector2D | null {
+    return this.bullet?.getPosition() || null;
+  }
+
+  isBulletActive(): boolean {
+    return this.bullet?.isShot() || false;
+  }
+
+  checkBulletOutOfBounds(width: number, height: number): boolean {
+    return this.bullet?.isOutOfBounds(width, height) || false;
+  }
+
+  resetBullet() {
+    this.bullet = null;
   }
 
   getPosition() {
@@ -51,6 +180,22 @@ class Player {
   getHeight() {
     return this.height;
   }
+
+  moveLeft() {
+    this.position.x -= 10;
+  }
+
+  moveRight() {
+    this.position.x += 10;
+  }
+
+  moveUp() {
+    this.position.y -= 10;
+  }
+
+  moveDown() {
+    this.position.y += 10;
+  }
 }
 
 class Game {
@@ -62,8 +207,11 @@ class Game {
   private maxPower: number;
   private degree: number;
   private maxDegree: number;
+  private powerDirection: number;
+  private wind: number;
 
   private player: Player;
+  private enemy: Enemy;
 
   constructor() {
     this.canvas = document.getElementById("canvas") as HTMLCanvasElement;
@@ -75,17 +223,52 @@ class Game {
     this.degree = 0;
     this.power = 0;
     this.maxDegree = 180;
-    this.maxPower = 100;
+    this.maxPower = 500;
+    this.powerDirection = 1;
+    this.wind = 0;
 
     window.addEventListener("keydown", (e) => {
-      if (e.key === " ") {
-        if (this.power < this.maxPower) this.power += 2;
+      if (e.key === "ArrowUp") {
+        if (this.degree < this.maxDegree) {
+          this.degree += 5;
+        }
+      } else if (e.key === "ArrowDown") {
+        if (this.degree > 0) {
+          this.degree -= 5;
+        }
+      } else if (e.key === " ") {
+        e.preventDefault();
+        if (this.power >= this.maxPower) {
+          this.powerDirection = -1;
+        } else if (this.power <= 0) {
+          this.powerDirection = 1;
+        }
+
+        if (this.powerDirection === 1) this.power += 10;
+        else if (this.powerDirection === -1) this.power -= 10;
+      } else if (e.key === "w") {
+        this.player.moveUp();
+      } else if (e.key === "a") {
+        this.player.moveLeft();
+      } else if (e.key === "s") {
+        this.player.moveDown();
+      } else if (e.key === "d") {
+        this.player.moveRight();
       }
     });
 
-    this.drawBackGround();
+    const shootButton = document.getElementById("shootButton");
+    shootButton?.addEventListener("click", () => {
+      if (this.player.shoot(this.degree, this.power, this.wind)) {
+        // Generate new random wind after successful shot
+        this.wind = (Math.random() - 0.5) * 2; // Random value between -1 and 1
+      }
+    });
 
     this.player = new Player(new Vector2D(100, this.canvas_height - 200));
+    this.enemy = new Enemy(
+      new Vector2D(this.canvas_width - 200, this.canvas_height - 200)
+    );
 
     this.gameLoop = this.gameLoop.bind(this);
     requestAnimationFrame(this.gameLoop);
@@ -98,34 +281,82 @@ class Game {
     }
   }
 
-  drawPlayer() {
+  drawWind() {
     if (this.ctx) {
-      const pos = this.player.getPosition();
-      const width = this.player.getWidth();
-      const height = this.player.getHeight();
-      this.ctx.fillStyle = "blue";
-      this.ctx.fillRect(pos.x, pos.y, width, height);
+      this.ctx.fillStyle = "white";
+      this.ctx.font = "20px Arial";
+      this.ctx.fillText(`${this.wind.toFixed(2)}`, 20, 30);
+
+      // Draw wind direction arrow
+      const arrowLength = Math.abs(this.wind) * 50;
+      const arrowX = 100;
+      const arrowY = 25;
+
+      this.ctx.beginPath();
+      this.ctx.strokeStyle = "white";
+      if (this.wind > 0) {
+        this.ctx.fillStyle = "green";
+        this.ctx.fillText(">>>>>>>>>>>", arrowX, arrowY + 5);
+      } else {
+        this.ctx.fillStyle = "green";
+        this.ctx.fillText("<<<<<<<<<<<", arrowX, arrowY + 5);
+      }
+      this.ctx.stroke();
     }
   }
 
   drawPower() {
     if (this.ctx) {
-      this.ctx.fillStyle = "green";
-      this.ctx.fillRect(240, this.canvas_height - 40, this.power * 10, 20);
+      this.powerDirection === 1
+        ? (this.ctx.fillStyle = "green")
+        : (this.ctx.fillStyle = "orange");
+      this.ctx.fillRect(240, this.canvas_height - 40, this.power * 2, 20);
 
       this.ctx.strokeStyle = "white";
-      this.ctx.strokeRect(240, this.canvas_height - 40, this.maxPower * 10, 20);
+      this.ctx.strokeRect(240, this.canvas_height - 40, this.maxPower * 2, 20);
     }
   }
 
   drawDegree() {
     if (this.ctx) {
+      const radius = 80;
       this.ctx.beginPath();
-      this.ctx.arc(120, this.canvas_height - 20, 80, Math.PI, 2 * Math.PI);
-      this.ctx.fillStyle = "transparent";
-      this.ctx.fill();
+      this.ctx.strokeStyle = "white";
+      this.ctx.arc(120, this.canvas_height - 20, radius, Math.PI, 2 * Math.PI);
       this.ctx.lineWidth = 5;
       this.ctx.stroke();
+
+      this.ctx.fillStyle = "black";
+      this.ctx.font = "20px arial";
+      this.ctx.fillText("90", 110, this.canvas_height - 80);
+      this.ctx.fillText("180", 44, this.canvas_height - 24);
+      this.ctx.fillText("0", 184, this.canvas_height - 24);
+
+      this.ctx.beginPath();
+      this.ctx.strokeStyle = "red";
+      const radian = (this.degree * Math.PI) / 180;
+      const centerX = 120;
+      const centerY = this.canvas_height - 22;
+      const x = centerX + radius * Math.cos(radian);
+      const y = centerY - radius * Math.sin(radian);
+      this.ctx.moveTo(centerX, centerY);
+      this.ctx.lineTo(x, y);
+      this.ctx.stroke();
+    }
+  }
+
+  checkCollisions() {
+    if (!this.ctx) return;
+
+    const bulletPos = this.player.getBulletPosition();
+    if (bulletPos && this.enemy.checkCollision(bulletPos)) {
+      this.enemy.takeDamage(20);
+      this.player.resetBullet();
+
+      if (this.enemy.isDead()) {
+        alert("You won!");
+        // Reset game or handle win condition
+      }
     }
   }
 
@@ -136,10 +367,22 @@ class Game {
   gameLoop() {
     this.clearCanvas();
 
-    this.drawPlayer();
+    // Update
+    this.player.update();
 
+    if (
+      this.player.checkBulletOutOfBounds(this.canvas_width, this.canvas_height)
+    ) {
+      this.player.resetBullet();
+    }
+
+    this.checkCollisions();
+
+    // Draw
+    this.drawWind();
+    this.player.draw(this.ctx!);
+    this.enemy.draw(this.ctx!);
     this.drawPower();
-
     this.drawDegree();
 
     requestAnimationFrame(this.gameLoop);
